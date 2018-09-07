1810f89badea12fece301291abf62c3389f99e0f
ravve01 <ravve01@RAVVE01-8X5MQ12.mshome.net>
2018-09-07 13:09:09 +0530

    Test Class

--- src/test/java/com/ca/test/TestClass.java
+++ src/test/java/com/ca/test/TestClass.java
@@ -5,11 +5,11 @@ import org.testng.annotations.Test;
 import com.ca.Division;
 
 public class TestClass {
-	/*@Test
+	@Test
 	public void test1(){
 		Division div=new Division();
 		div.divide(10, 0);
-	}*/
+	}
 	@Test
 	public void test2(){
 		Division div=new Division();
